7161b6e0ab394a9ae7cab5e8c17e2cf921c30604
liuxin <jh3513lx@163.com>
2020-10-28 15:52:58 +0800

    master

---
+++ Admin/controller/IndexController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Admin\controller;
+use Frame\libs\BaseController;
+use Admin\Model;
+final class indexController extends BaseController{
+    public function index(){
+        
+    }
+}
--- Frame/libs/BaseController.class.php
+++ Frame/libs/BaseController.class.php
@@ -15,6 +15,15 @@ abstract class BaseController{
         //smarty配置
         $smarty->left_delimiter = "<{";//左定界符
         $smarty->right_delimiter= "}>";//右定界符
-        
+        $smarty->setTemplateDir(VIEW_PATH);//视图文件目录
+        $smarty->setCompileDir(sys_get_temp_dir().DS."view");//编译文件目录
+        //给smarty属性赋值
+        $this->smarty = $smarty;
+    }
+    //跳转方法
+    protected function jump($message,$url='?',$time=3){
+        echo "<h2>{$message}</h2>";
+        header("refresh:{$time};url={$url}");
+        die();
     }
 }
