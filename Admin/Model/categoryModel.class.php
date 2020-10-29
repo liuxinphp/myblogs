182ce02382d9687120f8cdb5e307cb29695b8918
liuxin <jh3513lx@163.com>
2020-10-29 16:41:17 +0800

    master

--- Admin/Model/categoryModel.class.php
+++ Admin/Model/categoryModel.class.php
@@ -4,5 +4,8 @@ use \Frame\libs\BaseModel;
 use \Frame\Vendor\PDOWrapper;
 final class CategoryModel extends baseModel{
     protected $table = "category";
-    
+    //获取无限极分类数组
+    public function categoryList($arrs){
+        
+    }
 }
