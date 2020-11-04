ee3d991f961fdea5412443465f412b2fe393bfcf
liuxin <jh3513lx@163.com>
2020-11-04 14:51:27 +0800

    master

--- Admin/Model/articleModel.class.php
+++ Admin/Model/articleModel.class.php
@@ -3,14 +3,14 @@ namespace Admin\Model;
 use \Frame\Libs\BaseModel;
 final class articleModel extends BaseModel{
     protected $table = "article";
-    public function fetchAllWithJoin($where="2>1"){
+    public function fetchAllWithJoin($where="2>1",$startRow=0,$pageSize=10){
         //查询语句
         $sql = "select article.*,category.className,user.userName from {$this->table} ";
         $sql .="left join category on article.category_id = category.id ";
         $sql .="left join user on article.user_id = user.id ";
-        $sql .= "where {$where}";
-       // echo $sql;
-        //die();
+        $sql .= "where {$where} ";
+        $sql .= "order by article.id desc ";
+        $sql .= "limit {$startRow},{$pageSize}";
         return $this->pdo->fetchAll($sql);
     }
 }
