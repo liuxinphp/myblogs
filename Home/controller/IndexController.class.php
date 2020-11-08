630956b1e0cddf980e21e2071127fd47ff28df0d
liuxin <jh3513lx@163.com>
2020-11-08 21:55:07 +0800

    master

--- Home/controller/IndexController.class.php
+++ Home/controller/IndexController.class.php
@@ -6,8 +6,7 @@ final class IndexController extends BaseController{
     public function index(){
         //创建模型类对象
         $modelObj = IndexModel::getInstance();
-        //获取多行数据
-        $arrs = $modelObj->fetchAll();
+        $this->smarty->display("index/index.html");
         
     }
 }
