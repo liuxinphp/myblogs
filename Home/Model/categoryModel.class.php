16161cd872b024b0523d2d5e49f13588b9ed8f4d
liuxin <jh3513lx@163.com>
2020-11-09 11:30:06 +0800

    master

---
+++ Home/Model/categoryModel.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace Home\Model;
+use \Frame\Libs\BaseModel;
+final class categoryModel extends BaseModel{
+    protected $table = "category";
+    //获取文章数,带文章数据
+    
+
+    public function fetchAllWithCount(){
+        //构建sql语句
+        $sql = "select category.*,count(article.id) as records from {$this->table} ";
+        $sql.= "left join article on article.category_id=category.id ";
+        $sql.= "group by category.id";
+        return $this->pdo->fetchAll($sql);
+    }
+    //获取无限极分类
+    public function categoryList($arrs,$level=0,$pid=0){
+        static $categorys = array();
+        foreach($arrs as $arr){
+            if($arr['pid']==$pid){
+                $arr['level'] = $level;//菜单层级
+                $categorys[] = $arr;
+                $this->categoryList($arrs,$level+1,$arr['id']);
+            }
+        }
+        return $categorys;
+    }
+}
--- Home/controller/IndexController.class.php
+++ Home/controller/IndexController.class.php
@@ -1,11 +1,22 @@
 <?php
 namespace Home\controller;
+use \Home\Model\linkModel;
 use \Home\Model\IndexModel;
 use \Frame\libs\BaseController;
+use Home\Model\categoryModel;
+
 final class IndexController extends BaseController{
     public function index(){
-        //创建模型类对象
-        $modelObj = IndexModel::getInstance();
+        //(1)获取友情链接数据
+        $links = linkModel::getInstance()->fetchAll();
+        //(2)获取文章分类数据
+        $categorys = categoryModel::getInstance()->categoryList(
+            //获取无限极分类数据
+            categoryModel::getInstance()->fetchAllWithCount());
+        $this->smarty->assign(array(
+            "links"=>$links,
+            "categorys"=>$categorys
+        ));
         $this->smarty->display("index/index.html");
         
     }
